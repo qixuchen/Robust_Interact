decb7df1ec2e5320af7724acbc6a3a4a8c1caa32
qchenax <qchenax@gmail.com>
2021-12-11 21:07:20 +0800

    Add some testing data

--- Code/PointPrune/main.cpp
+++ Code/PointPrune/main.cpp
@@ -21,6 +21,7 @@ int top_1_found=0;
 
 int main(int argc, char *argv[])
 {
+    srand(time(0));  // Initialize random number generator.
     double HD_s=0;
     double HD_s_rr=0;
     int HD_s_count = 0;
@@ -50,14 +51,14 @@ int main(int argc, char *argv[])
     int k =1;
     int checknum=3;
     double theta=0.05;
+    int num_repeat = 100;
     std::vector<point_t *> p_set, p0;
     skyband(P0, p_set, k);
     point_set_t *P = point_reload(p_set);
 
 
-    for(int i=0; i<10; i++){
+    for(int i=0; i<num_repeat; i++){
         // generate the utility vector
-        srand(time(0));  // Initialize random number generator.
         point_t *u = alloc_point(dim);
         double sum = 0;
         for (int i = 0; i < dim; i++)
@@ -68,7 +69,7 @@ int main(int argc, char *argv[])
         for (int i = 0; i < dim; i++)
             u->coord[i] = u->coord[i]/sum;
 
-
+        
         // look for the ground truth top-k point
         std::vector<point_t *> top_current;
         find_top_k(u, P, top_current, k);
@@ -79,44 +80,45 @@ int main(int argc, char *argv[])
         printf("---------------------------------------------------------\n");
         top_1_score = dot_prod(u,top_current[0]);
 
-        //Algorithm HDPI_sampling
+        //Algorithm HDPI_sampling (11.35, 0.78)
         HD_s += HDPI_sampling(p_set, u, k, theta);
         HD_s_rr += rr_ratio;
         HD_s_count += top_1_found;
 
-        //Algorithm HDPI_accurate
+        //Algorithm HDPI_accurate (11.38, 0.77)
         HD_a += HDPI_accurate(p_set, u, k, theta);
         HD_a_rr += rr_ratio;
         HD_a_count += top_1_found;
 
-        //Algorithm PointPrune
+        //Algorithm PointPrune  (0.4, 20.5, 0.88)
+        //                        (0.1, 25, 0.94)
         PP += PointPrune(p_set, u, k, checknum, theta);
         PP_rr += rr_ratio;
         PP_count += top_1_found;
 
-        //Algorithm PointPrune_v2
+        //Algorithm PointPrune_v2 (0.4, 20, 0.9)
         PP_2 += PointPrune_v2(p_set, u, k, checknum, theta);
         PP_2_rr += rr_ratio;
         PP_2_count += top_1_found;
 
-        //Algorithm SamplePrune
+        //Algorithm SamplePrune (0.4, 21.2 ,0.93)
         SP += SamplePrune(p_set, u, k, checknum, theta);
         SP_rr += rr_ratio;
         SP_count += top_1_found;
 
-        //Algorithm SamplePrune
+        //Algorithm STMD (2, 17.2, 0.89)
         ST += STMD(p_set, u, k, theta);
         ST_rr += rr_ratio;
         ST_count += top_1_found;
     }
     printf("\nIn total\n");
     printf("|%20s |%10s |%8s |%10s\n", "Alg", "# rounds", "rr", "found top1");
-    printf("|%20s |%10f |%8f |%8f\n", "HDPI-Sampling", HD_s/10, HD_s_rr/10, ((double)HD_s_count)/10);
-    printf("|%20s |%10f |%8f |%8f\n", "HDPI-Accurate", HD_a/10, HD_a_rr/10, ((double)HD_a_count)/10);
-    printf("|%20s |%10f |%8f |%8f\n", "PointPrune", PP/10, PP_rr/10, ((double)PP_count)/10);
-    printf("|%20s |%10f |%8f |%8f\n", "PointPrune_v2", PP_2/10, PP_2_rr/10, ((double)PP_2_count)/10);
-    printf("|%20s |%10f |%8f |%8f\n", "SamplePrune", SP/10, SP_rr/10, ((double)SP_count)/10);
-    printf("|%20s |%10f |%8f |%8f\n", "STMD", ST/10, ST_rr/10, ((double)ST_count)/10);
+    printf("|%20s |%10f |%8f |%8f\n", "HDPI-Sampling", HD_s/num_repeat, HD_s_rr/num_repeat, ((double)HD_s_count)/num_repeat);
+    printf("|%20s |%10f |%8f |%8f\n", "HDPI-Accurate", HD_a/num_repeat, HD_a_rr/num_repeat, ((double)HD_a_count)/num_repeat);
+    printf("|%20s |%10f |%8f |%8f\n", "PointPrune", PP/num_repeat, PP_rr/num_repeat, ((double)PP_count)/num_repeat);
+    printf("|%20s |%10f |%8f |%8f\n", "PointPrune_v2", PP_2/num_repeat, PP_2_rr/num_repeat, ((double)PP_2_count)/num_repeat);
+    printf("|%20s |%10f |%8f |%8f\n", "SamplePrune", SP/num_repeat, SP_rr/num_repeat, ((double)SP_count)/num_repeat);
+    printf("|%20s |%10f |%8f |%8f\n", "STMD", ST/num_repeat, ST_rr/num_repeat, ((double)ST_count)/num_repeat);
 
     release_point_set(P, true);
     return 0;
