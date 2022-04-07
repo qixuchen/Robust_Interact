96ca002f37e2b0d8fc68dc9a6b7a607cef4ad1f6
qchenax <qchenax@gmail.com>
2022-04-07 15:47:46 +0800

    remove unnecessary directory user study motivation

--- Code/User_study(motivation)/RH.h
+++
@@ -1,3 +0,0 @@
-#include "Others/used_function.h"
-
-void RH(FILE *wPtr, std::vector<point_t*> &p_set, point_set_t* original_P, int k, int interval, point_t* u, double *numQ, vector<point_t*> *rePoint);
